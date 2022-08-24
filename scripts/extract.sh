a47e603b466e6308968906ffa74e5ec18ec82e0b
dariusxdragoi <105705622+dariusxdragoi@users.noreply.github.com>
2022-08-24 16:13:08 +0300

    Update extract.sh

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,18 +1,19 @@
 #/usr/bin/env bash
 org="ftos-forks"
+file="rrepos.json"
 limit=5
 
 gh repo list ${org} --json name --limit ${limit} | jq '.[].name' | tr -d '"'
 
-gh repo list ${org} --json name --limit ${limit} | jq '.[].name' | tr -d '"'
+gh repo list ${org} --json name --limit ${limit} | jq '.[].name' | tr -d '"' > ${file}
 
 while IFS= read -r line
 do
   x=$(curl -u dariusxdragoi:acceces https://api.github.com/repos/ftos-forks/${line}) 
-  echo $x > ./info.json
+  cat $x > ./info.json
   y=$(jq '.parent.full_name' info.json | tr -d '"')
   if [ "$y" != "null" ]
   then
     echo "ftos-forks/$line is forked from $y"
   fi
-done
+done < ${file}
